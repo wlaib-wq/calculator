37c34fce7397386ada7d864d51d94aaa264044eb
wlain <wlaincornit@gmail.com>
2025-01-04 23:25:01 +0000

    js

---
+++ calculator/js.js
@@ -0,0 +1,17 @@
+let number = document.getElementById('calculator_number')
+let value = document.getElementById('val')
+let num = document.getElementById("num")
+let input
+
+function number_calculator(){
+    value.value = number.innerText
+    input = number.innerText
+    input = value.value
+    console.log(input)
+}
+
+function numbers(){
+    switch{
+        
+    }
+}
