d0890e7f9cc98fd17da6c831734c6d11e5393870
wlain <wlaincornit@gmail.com>
2025-01-05 16:56:54 +0000

    Correct js code

--- calculator/js.js
+++ calculator/js.js
@@ -1,17 +1,25 @@
-let number = document.getElementById('calculator_number')
-let value = document.getElementById('val')
-let num = document.getElementById("num")
-let input
-
-function number_calculator(){
-    value.value = number.innerText
-    input = number.innerText
-    input = value.value
-    console.log(input)
+function addToDisplay(value){
+    document.getElementById("display").value += value;
 }
 
-function numbers(){
-    switch{
-        
+function clearDisplay() {
+    document.getElementById('display').value = '';
+    document.getElementById('display').style.backgroundColor ='white'
+}
+
+function calculateResult() {
+    try {
+// Получение результата выражения и вывод результата на дисплей.
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+        document.getElementById('display').style.backgroundColor ='rgba(14, 179, 14, 0.262)'
+    } catch (error) {
+// Если в процессе вычисления возникла ошибка, на дисплее отобразится сообщение 'Error'.
+        document.getElementById('display').value = 'Error';
+        document.getElementById('display').style.backgroundColor ='rgba(179, 14, 14, 0.26)'
     }
+
+    if (document.getElementById('display').value == "undefined"){
+        document.getElementById('display').style.backgroundColor ='rgba(179, 14, 14, 0.26)'
+    }
+
 }
